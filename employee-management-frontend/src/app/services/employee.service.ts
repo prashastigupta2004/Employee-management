1086bf5a5dad3971db522fe7a3ce1108f40cdd3f
Prashasti Gupta <118652336+prashastigupta2004@users.noreply.github.com>
2025-10-07 11:34:56 +0530

    Update employee.service.ts

--- employee-management-frontend/src/app/services/employee.service.ts
+++ employee-management-frontend/src/app/services/employee.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../env/env';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private baseUrl = 'http://localhost:5000/api/employees';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
