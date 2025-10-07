dafed52c94dbe1baeb56946d340a44acfaf18cbe
Prashasti Gupta <118652336+prashastigupta2004@users.noreply.github.com>
2025-10-07 11:40:58 +0530

    Update index.js

--- employee-management-backend/index.js
+++ employee-management-backend/index.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://employee-management-3-p6kw.onrender.com',
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
